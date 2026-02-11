8ab3f2c64a12993e68a540954eeb58c1a4a95ee6
kimchinh1990 <trankimchinh20240620@gmail.com>
2026-02-11 15:28:46 +0700

    Day 04: Bài tập if-elif-else phân loại điểm

---
+++ daily_Python/day04.py
@@ -0,0 +1,8 @@
+score = float(input("Hãy nhập điểm thi của bạn:"))
+
+if score >= 8:
+    print("Chúc mừng bạn đạt loại Giỏi")
+elif score >= 6:
+    print("Bạn chỉ đạt loại Khá")
+else:
+    print("Bạn cần cố gắng thêm!")
